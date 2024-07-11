6edf11df6ea5146d50a51ea99c6d103182a8e20e
BHARTI RAWAT <bhartirawatbr26@gmail.com>
2024-07-11 18:57:26 +0530

    update

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+  document.write("hello js");
+  var x=10;
+  document.write(x);
+//   old way to create a variable
+// in  modern we use let keyward for making a variable
+
+let y=2;
+document.write(y);
+let age=10;
+console.log(age);
+let BhartiRawat=true;
+document.write("<br>"+BhartiRawat);
+
+let a="heavy";
+let b=45;
+document.write("<br>",a - b);
+// array
+
+let number=[1,2,3,4,5];
+let fruit=["apple","banana","orange"];
+document.write("<br>",number[4]);
